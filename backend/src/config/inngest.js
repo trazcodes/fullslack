359a1440804e3dabb8ced468e6c85136c089b204
kush <kushal27001@gmail.com>
2025-12-04 20:35:56 +0530

    fix: correct import statement for database connection in inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import { connectDB } from "./db";
+import { connectDB } from "./db.js";
 import User from "../models/user.model.js";
 
 // Create a client to send and receive events
