f641da007a7d1b18be4c98a0fa5cfe9448087393
kush <kushal27001@gmail.com>
2025-12-04 20:42:42 +0530

    fix: correct import statement for User model in inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "../models/user.model.js";
+import {User} from "../models/user.model.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "fullslack" });
