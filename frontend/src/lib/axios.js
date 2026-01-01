69343c1fec97e453f234baf89aaad2e5f6a88040
kush <kushal27001@gmail.com>
2026-01-01 23:36:41 +0530

    feat: set up Docker environment with Dockerfile and docker-compose.yml, update axios base URL for development

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 
     import.meta.env.MODE == 'development'
-    ? "http://localhost:5000/api"
+    ? "http://localhost:8080/api"
     : "https://fullslack-backend.vercel.app/api";
 
 export const axiosInstance = axios.create({
